b605a0fb8d1a9eadc0183dda863d291d1b2716f2
JINGHE CHEN <1105211099@gap.wzu.edu.tw>
2020-02-21 11:27:21 +0800

    Initial commit

---
+++ Assets/Script/AudioVisualization.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioVisualization : MonoBehaviour
+{
+    public AudioSource _audioSource;
+
+    //麥克風輸入
+    public bool usemicrophone;
+    public AudioClip m_audioclip;
+
+    public string SelectedDevice;
+
+    public static float[] _samples = new float[512];
+    public void Start()
+    {
+        if (usemicrophone)
+        {
+            if (Microphone.devices.Length > 0)
+            {
+                SelectedDevice = Microphone.devices[0].ToString();
+                _audioSource.clip = Microphone.Start(SelectedDevice, true, 10, AudioSettings.outputSampleRate);
+            }
+            else
+            {
+                usemicrophone = false;
+            }
+
+        }
+        if (!usemicrophone)
+        {
+            _audioSource.clip = m_audioclip;
+        }
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ Assets/Script/InstantiateEffects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateEffects : MonoBehaviour
+{
+    public GameObject EffectPrefab;
+    public GameObject[] sample_Effects = new GameObject[512];
+    public float _MaxScale;
+
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject _intantanceSampleEffect = (GameObject)Instantiate(EffectPrefab);
+            _intantanceSampleEffect.transform.position = this.transform.position;
+            _intantanceSampleEffect.transform.parent = this.transform;
+            _intantanceSampleEffect.name = "SampleEffect" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _intantanceSampleEffect.transform.position = Vector3.forward * 100;
+            sample_Effects[i] = _intantanceSampleEffect;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (sample_Effects != null)
+            {
+                sample_Effects[i].transform.localScale = new Vector3(10, (AudioVisualization._samples [i] * _MaxScale) +2, 10);
+            }
+        }
+    }
+}
